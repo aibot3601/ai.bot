ae33c70e46a4c68e3d10e468bb5fbd63ea0c5f7e
javsalas <javsalas@gmail.com>
2023-11-03 23:31:48 -0400

    Setting original 5

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -116,6 +116,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+LOGIN_URL = '/signin'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
