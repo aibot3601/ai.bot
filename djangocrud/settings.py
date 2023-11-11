e1ae560d4616009b26d31ddd54191605b45eb111
javsalas <javsalas@gmail.com>
2023-11-10 22:01:22 -0400

    Clave secreta

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='clave secreta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
